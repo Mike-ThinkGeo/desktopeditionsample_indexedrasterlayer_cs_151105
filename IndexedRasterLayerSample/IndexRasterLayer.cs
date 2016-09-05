85a0799dffc9c9e58ac98b3fa753a10448951c20
sicnuthinkgeo <1270896784@qq.com>
2016-09-05 19:59:15 +0800

    first commit

---
+++ IndexedRasterLayerSample/IndexRasterLayer.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+
+namespace ThinkGeo.MapSuite.Core
+{
+    public class IndexRasterLayer : RasterLayer
+    {
+        private bool isOpen;
+        private string idxPathFileName;
+        private RtreeSpatialIndex rTreeIndex;
+        private Collection<MrSidRasterLayer> rasterLayers;
+
+        public IndexRasterLayer()
+            : this(new Collection<MrSidRasterLayer>(), string.Empty)
+        { }
+
+        public IndexRasterLayer(string idxPathFileNam)
+     : this(new Collection<MrSidRasterLayer>(), idxPathFileNam)
+        { }
+
+        public IndexRasterLayer(IEnumerable<MrSidRasterLayer> rasterLayers)
+            : this(rasterLayers, string.Empty)
+        { }
+
+        public IndexRasterLayer(IEnumerable<MrSidRasterLayer> rasterLayers, string idxPathFileName)
+            : base()
+        {
+            this.idxPathFileName = idxPathFileName;
+
+            this.rasterLayers = new Collection<MrSidRasterLayer>();
+            foreach (var layer in rasterLayers)
+            {
+                this.rasterLayers.Add(layer);
+            }
+        }
+
+        public string IndexFilePath
+        {
+            set { this.idxPathFileName = value; }
+            get { return this.idxPathFileName; }
+        }
+
+        public Collection<MrSidRasterLayer> RasterLayers
+        {
+            set { this.rasterLayers = value; }
+            get { return this.rasterLayers; }
+        }
+
+
+        public override bool HasBoundingBox
+        {
+            get { return true; }
+        }
+
+        protected override bool IsOpenCore
+        {
+            get { return isOpen; }
+        }
+
+        protected override void OpenCore()
+        {
+            if (!IsOpenCore)
+            {
+                isOpen = true;
+
+                if (!File.Exists(idxPathFileName))
+                {
+                    RtreeSpatialIndex.CreateRectangleSpatialIndex(idxPathFileName, RtreeSpatialIndexPageSize.EightKilobytes, RtreeSpatialIndexDataFormat.Float);
+
+                    rTreeIndex = new RtreeSpatialIndex(idxPathFileName, RtreeSpatialIndexReadWriteMode.ReadWrite);
+                    rTreeIndex.Open();
+
+                    foreach (var rasterLayer in rasterLayers)
+                    {
+                        rasterLayer.Open();
+                        RectangleShape boudingBox = rasterLayer.GetBoundingBox();
+
+                        Feature feature = new Feature(boudingBox);
+                        feature.Id = rasterLayer.PathFilename;
+                        rTreeIndex.Add(feature);
+                    }
+                }
+                else
+                {
+                    rTreeIndex = new RtreeSpatialIndex(idxPathFileName, RtreeSpatialIndexReadWriteMode.ReadOnly);
+                    rTreeIndex.Open();
+                }
+            }
+        }
+
+        protected override void DrawCore(GeoCanvas canvas, Collection<SimpleCandidate> labelsInAllLayers)
+        {
+            Collection<string> ids = rTreeIndex.GetFeatureIdsIntersectingBoundingBox(canvas.CurrentWorldExtent);
+
+            var layers = rasterLayers.Where(a => ids.Contains(a.PathFilename));
+
+            foreach (var layer in layers)
+            {
+                layer.Open();
+                layer.Draw(canvas, labelsInAllLayers);
+            }
+        }
+
+        protected override RectangleShape GetBoundingBoxCore()
+        {
+            Collection<RectangleShape> boundingBoxs = new Collection<RectangleShape>();
+
+            foreach (var rasterLayer in rasterLayers)
+            {
+                rasterLayer.Open();
+                boundingBoxs.Add(rasterLayer.GetBoundingBox());
+            }
+
+            return ExtentHelper.GetBoundingBoxOfItems(boundingBoxs);
+        }
+    }
+}
---
+++ IndexedRasterLayerSample/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using ThinkGeo.MapSuite.Core;
+using ThinkGeo.MapSuite.WpfDesktopEdition;
+
+namespace IndexedRasterLayerSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            map.MapUnit = GeographyUnit.Meter;
+        }
+
+        private void LoadButtonClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbFolder.Text = folderBrowserDialog.SelectedPath;
+            }
+
+            if (!string.IsNullOrEmpty(tbFolder.Text))
+            {
+                IndexRasterLayer indexRasterLayer = new IndexRasterLayer(@"..\..\index.idx");
+                string[] files = Directory.GetFiles(tbFolder.Text);
+
+                foreach (var file in files)
+                {
+                    indexRasterLayer.RasterLayers.Add(new MrSidRasterLayer(file));
+                }
+
+                LayerOverlay layerOverlay = new LayerOverlay();
+                layerOverlay.Layers.Add(indexRasterLayer);
+                map.Overlays.Add(layerOverlay);
+
+                indexRasterLayer.Open();
+                map.CurrentExtent = indexRasterLayer.GetBoundingBox();
+
+                map.Refresh();
+            }
+        }
+    }
+}
